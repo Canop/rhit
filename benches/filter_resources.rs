a0394be2181e97ec678d68b1286756bc2087b478
Canop <cano.petrole@gmail.com>
2022-05-18 08:06:13 +0200

    fix bench compilation

--- benches/filter_resources.rs
+++ benches/filter_resources.rs
@@ -9,10 +9,10 @@ use {
 fn filter_resources(bench: &mut Bench) {
     bench.task("filter resources", |task| {
         let package_dir = std::env::var_os("CARGO_MANIFEST_DIR").expect("manifest dir not set");
-        let path = PathBuf::from(package_dir).join("test-data/");
+        let paths = vec![PathBuf::from(package_dir).join("test-data/")];
         let mut args = Args::default();
         args.silent_load = true;
-        let base = LogBase::new(&path, &args).unwrap();
+        let base = LogBase::new(&paths, &args).unwrap();
         assert_eq!(base.lines.len(), 33468);
         task.iter(|| {
             let count = base.lines.iter()
--- benches/load_base.rs
+++ benches/load_base.rs
@@ -9,11 +9,11 @@ use {
 fn bench_base_loading(bench: &mut Bench) {
     bench.task("read access.log", |task| {
         let package_dir = std::env::var_os("CARGO_MANIFEST_DIR").expect("manifest dir not set");
-        let path = PathBuf::from(package_dir).join("test-data/");
+        let paths = vec![PathBuf::from(package_dir).join("test-data/")];
         let mut args = Args::default();
         args.silent_load = true;
         task.iter(|| {
-            let base = LogBase::new(&path, &args).unwrap();
+            let base = LogBase::new(&paths, &args).unwrap();
             assert_eq!(base.lines.len(), 33468);
             pretend_used(base);
         });
