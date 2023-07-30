472bbde2c0f609b69527ae45bd31ff9392246336
Canop <cano.petrole@gmail.com>
2023-07-30 17:05:07 +0200

    oops

---
+++ src/json.rs
@@ -0,0 +1,63 @@
+use {
+    crate::*,
+    anyhow::*,
+    std::{
+        path::PathBuf,
+    },
+};
+
+/// A printer writing lines as a JSON array
+#[derive(Default)]
+struct JsonPrinter {
+    written: usize,
+}
+
+impl LineConsumer for JsonPrinter {
+    fn eat_line(
+        &mut self,
+        line: LogLine,
+        _raw_line: &str,
+        filtered_out: bool,
+    ) {
+        if filtered_out { return; }
+        if self.written > 0 {
+            print!(", ");
+        } else {
+            println!("\n[");
+        }
+        print!(r#"  {{
+    "date": "{}",
+    "remote_addr": "{}",
+    "method": "{}",
+    "path": "{}",
+    "status": "{}",
+    "bytes_sent": {},
+    "referer": "{}"
+  }}"#,
+            line.date,
+            line.remote_addr,
+            line.method,
+            line.path,
+            line.status,
+            line.bytes_sent,
+            line.referer,
+        );
+        self.written += 1;
+    }
+    fn end_eating(
+        &mut self,
+    ) {
+        println!("\n]");
+    }
+}
+
+pub fn print_json_lines(
+    path: &[PathBuf],
+    args: &args::Args,
+) -> Result<()> {
+    let mut printer = JsonPrinter::default();
+    let mut file_reader = FileReader::new(path, args, &mut printer)?;
+    time!("reading files", file_reader.read_all_files())?;
+    Ok(())
+}
+
---
+++ src/raw.rs
@@ -0,0 +1,40 @@
+use {
+    crate::*,
+    anyhow::*,
+    std::{
+        path::PathBuf,
+    },
+};
+
+
+struct RawPrinter {
+}
+
+impl LineConsumer for RawPrinter {
+    fn start_eating(
+        &mut self,
+        _first_date: Date,
+    ) {
+        // nothing to do here
+    }
+    fn eat_line(
+        &mut self,
+        _log_line: LogLine,
+        raw_line: &str,
+        filtered_out: bool,
+    ) {
+        if !filtered_out {
+            print!("{}", raw_line);
+        }
+    }
+}
+
+pub fn print_raw_lines(
+    path: &[PathBuf],
+    args: &args::Args,
+) -> Result<()> {
+    let mut printer = RawPrinter{};
+    let mut file_reader = FileReader::new(path, args, &mut printer)?;
+    time!("reading files", file_reader.read_all_files())?;
+    Ok(())
+}
