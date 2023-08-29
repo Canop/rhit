fcba3e9051fd39b0b83855863f469257f265bd85
Canop <cano.petrole@gmail.com>
2023-08-29 16:56:32 +0200

    underline doc url in --help

--- src/cli/help.rs
+++ src/cli/help.rs
@@ -3,13 +3,17 @@ use {
         args::*,
     },
     clap::CommandFactory,
-    termimad::ansi,
+    termimad::{
+        ansi,
+        CompoundStyle,
+        crossterm::style::{Attribute, Color},
+    },
 };
 
 static INTRO_TEMPLATE: &str = "
 **Rhit** analyses your nginx logs.
 
-Complete documentation at https://dystroy.org/rhit
+Complete documentation at *https://dystroy.org/rhit*
 
 ";
 
@@ -19,6 +23,7 @@ pub fn print() {
         .without("author");
     let skin = printer.skin_mut();
     skin.headers[0].compound_style.set_fg(ansi(204));
-    skin.bold.set_fg(ansi(204));
+    skin.italic = CompoundStyle::with_attr(Attribute::Underlined);
+    skin.bold.set_fg(Color::AnsiValue(204));
     printer.print_help();
 }
